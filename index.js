480c6083461b7c8adfa3e4b4b552baa58b532f3b
rema-salman <rema.salman@outlook.com>
2021-05-09 15:51:40 +0200

    Testing CD/CI

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://0.0.0.0:${port}`)
 })
