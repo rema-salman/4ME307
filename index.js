0539be03ca168e45cb4ea8c64bb8cd80514e0357
rema-salman <rema.salman@outlook.com>
2021-05-09 15:48:15 +0200

    Testing CD/CI

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(JSON.stringify({
+	"result":"Hello World!",
+	"status":200
+	}))
 })
 
 app.listen(port, () => {
