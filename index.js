3f508aa199b311330157560f1f2730397051487b
rema-salman <rema.salman@outlook.com>
2021-05-11 05:03:04 +0200

    Testing CD/CI

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send(JSON.stringify({
-	"result":"Hello World!!",
-	"status":200
-	}))
-})
+// app.get('/', (req, res) => {
+//   res.send(JSON.stringify({
+// 	"result":"Hello World!!",
+// 	"status":200
+// 	}))
+// })
 
+// Deliver HTML Files with Express via sendFile
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://0.0.0.0:${port}`)
-})
+  console.log(`Example app listening at http://0.0.0.0:${port}`);
+});
