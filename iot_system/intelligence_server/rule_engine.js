d2cfa33e9b2aef27e644b3a3ea5f41b875aec6e3
rema-salman <rema.salman@outlook.com>
2021-05-27 14:58:35 +0200

    bug fixed in condition

--- iot_system/intelligence_server/rule_engine.js
+++ iot_system/intelligence_server/rule_engine.js
@@ -21,45 +21,48 @@ function changeIrrigationStatus(motor_instance) {
 // access the latesed retrieved data from fields_station
 // turn motors off, then check if moisture is low and trigger field motor
 function checkMoistureAndWater() {
-  FirebaseDB.ref("fields_station")
-    .limitToLast(1)
-    .on("value", function (snapshot) {
-      snapshot.forEach(function (childSnapshot) {
-        const latestFieldsReadigns = childSnapshot.val();
+  FirebaseDB.ref("fields_station").on("value", function (snapshot) {
+    const soil_moisture_list = [];
+    const levels = snapshot.val();
+    for (i in snapshot.val()) {
+      soil_moisture_list.push(levels[i]);
+      console.log(soil_moisture_list);
+    }
+    // set both motors to off before the check
+    setIrrigationStatus();
+    //--------------- Low moisture of field One ---------------
+    if (
+      soil_moisture_list[soil_moisture_list.length - 1].field_1.soil_moisture <=
+      30
+    ) {
+      changeIrrigationStatus(FirebaseDB.ref("irrigation_status/motor_one"));
 
-        // set both motors to off before the check
-        setIrrigationStatus();
+      // pushes a new date whenever irrigation is on
+      FirebaseDB.ref("irrigation_status_track/motor_one")
+        .push()
+        .set(new Date().toISOString())
+        .catch((error) => {
+          console.log("caught", error.message);
+        });
+    }
 
-        //--------------- Low moisture of field one ---------------
-        console.log("field 1:" + latestFieldsReadigns.field_1.soil_moisture);
-        if (latestFieldsReadigns.field_1.soil_moisture <= 30) {
-          changeIrrigationStatus(FirebaseDB.ref("irrigation_status/motor_one"));
-
-          // pushes a new date whenever irrigation is on
-          FirebaseDB.ref("irrigation_status_track/motor_one")
-            .push()
-            .set(new Date().toISOString())
-            .catch((error) => {
-              console.log("caught", error.message);
-            });
-        }
-
-        //--------------- Low moisture of field two ---------------
-        console.log("field 2:" + latestFieldsReadigns.field_2.soil_moisture);
-        if (latestFieldsReadigns.field_2.soil_moisture <= 30) {
-          fields_station.changeIrrigationStatus(
-            FirebaseDB.ref("irrigation_status/motor_two")
-          );
-          // pushes a new date whenever irrigation is on
-          FirebaseDB.ref("irrigation_status_track/motor_two")
-            .push()
-            .set(new Date().toISOString())
-            .catch((error) => {
-              console.log("caught", error.message);
-            });
-        }
-      });
-    });
+    //--------------- Low moisture of field two ---------------
+    if (
+      soil_moisture_list[soil_moisture_list.length - 1].field_2.soil_moisture <=
+      30
+    ) {
+      fields_station.changeIrrigationStatus(
+        FirebaseDB.ref("irrigation_status/motor_two")
+      );
+      // pushes a new date whenever irrigation is on
+      FirebaseDB.ref("irrigation_status_track/motor_two")
+        .push()
+        .set(new Date().toISOString())
+        .catch((error) => {
+          console.log("caught", error.message);
+        });
+    }
+  });
 }
 
 module.exports = { checkMoistureAndWater };
